5fbbcce73ae87060fff41903a3c26e7645f7241e
suijing <sj178220709@163.com>
2013-05-20 19:55:28 +0800

    sqlitesys被转移到其他项目中了

--- Test.cs
+++ Test.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Threading;
-using Consoletest001;
-using Consoletest001.Delegate;
-using Consoletest001.NewFolder1;
-using Consoletest001.Sqlite;
-using Consoletest001.Test;
-using Consoletest001.WEB;
-using Consoletest001.jiami;
-using Consoletest001.otherThing;
-using Consoletest001.thread1;
-using Consoletest001.threadGaoji;
+﻿using Consoletest001.image;
 
 internal class Test
 {
 
     private static void Main()
     {
-
+        ImageDemo.maingsdf();
     
-        SQLTest.maindf();
+        //SQLTest.maindf();
       
     }
 
