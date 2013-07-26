e2908029a4b29bcc1aca484b29bc770bd9081fac
suijing <sj178220709@163.com>
2013-07-27 00:18:07 +0800

    增加mania音乐文件管理模块

---
+++ ManiaSongs/SongInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Consoletest001.ManiaSongs
+{
+    class SongInfo
+    {
+        public string BmsName { get; set; }
+
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public string PlayLevel { get; set; }
+
+        public string SongName { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string IconName { get; set; }
+
+    }
+}
--- Test.cs
+++ Test.cs
@@ -1,21 +1,22 @@
-﻿using Consoletest001.MyGame;
+﻿using Consoletest001.ManiaSongs;
+using Consoletest001.MyGame;
 using Consoletest001.image;
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
-
+using Consoletest001.otherThing;
 
 
 internal class Test
 {
     private static void Main()
     {
-        string str = ComputerInfo.ComputerInfoHead;
-
-        ThreeGate.t1();
+       // StringTest.StringTest1();
+//        string path = @"F:\Mania\bmg_bak" + "\\" + @"(班德瑞) 童年的记忆.bms";
+//        SongInfo info = SongDal.GetInfoFromPath(path);
+        CommonDictionary.MainTest();
     }
 }
     
