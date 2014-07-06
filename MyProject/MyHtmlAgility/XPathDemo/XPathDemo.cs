ca7c6038e0ed8ddb78b7d88130f99d3c755dcaa3
jsonsong <jsonsong@hotmail.com>
2014-07-06 23:56:29 +0800

    xPath demo (not complete)

---
+++ MyProject/MyHtmlAgility/XPathDemo/XPathDemo.cs
@@ -0,0 +1,34 @@
+﻿#region Using namespace
+
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace MyProject.MyHtmlAgility.XPathDemo
+{
+    [TestClass]
+    public class XPathDemo
+    {
+        public  static string GetemoXML
+        {
+            get
+            {
+                return "";
+            }
+
+           
+        }
+
+        public void main1()
+        {
+            var web = new HtmlDocument();
+            web.LoadHtml(GetemoXML);
+            var root = web.DocumentNode;
+            var chld = root.SelectNodes("");
+
+
+        }
+
+    }
+}
