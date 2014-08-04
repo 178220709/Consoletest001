09fb8643a2a699c7189db3f542c3f50456edc3ae
suijing <sj178220709@163.com>
2014-08-05 01:03:18 +0800

    js Demo(not  complete )

---
+++ MyMvcDemo/Controllers/MyJSDemo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MyMvcDemo.Extend;
+using MyProject.WeixinModel.Model;
+
+namespace MyMvcDemo.Controllers
+{
+
+    public class MyJSDemo : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
