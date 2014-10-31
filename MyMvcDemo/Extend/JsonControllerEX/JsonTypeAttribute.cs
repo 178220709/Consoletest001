0d760fe85c34f4d8771416d13099333327c977db
jsonsong <sj178220709@163.com>
2014-10-31 09:57:02 +0800

    1

---
+++ MyMvcDemo/Extend/JsonControllerEX/JsonTypeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Helpers;
+
+namespace MyMvcDemo.Extend
+{
+    /// <summary>
+    /// 标记该controller和里面的action方法是否用于首页菜单
+    /// </summary>
+    public class JsonTypeAttribute : Attribute
+    {
+        public JsonTypeAttribute(JsonType type)
+        {
+            JsonType = type;
+        }
+
+        public JsonType JsonType { get; set; } 
+
+
+    }
+}
