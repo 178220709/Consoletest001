9bf9bb88f51546b11e9ab4dd97514babdc6e89af
suijing <sj178220709@163.com>
2015-01-13 23:33:27 +0800

    1

--- MyMvcDemo/Scripts/treeUtil.js
+++
@@ -1,7 +0,0 @@
-﻿$.extend($.fn.tree.methods, {
-    unSelect: function (jq, target) {
-        return jq.each(function () {
-            $(target).removeClass("tree-node-selected");
-        });
-    }
-});
