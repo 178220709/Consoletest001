35760fb1ccbde48e383a155a483756d65c025658
suijing <sj178220709@163.com>
2015-01-27 22:03:18 +0800

    1

---
+++ Utils/sysTools/CollectionHepler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Omu.ValueInjecter;
+
+namespace Suijing.Utils.sysTools
+{
+    public static  class CollectionEx
+    {
+        public static bool HasValue<T>(this IEnumerable<T> list)
+        {
+            return (list != null && list.Any());
+        }
+
+    }
+}
