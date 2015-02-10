cb422c0caae1363de27631c8308b42d4d58918cd
suijing <sj178220709@163.com>
2015-02-10 22:56:30 +0800

    ref

---
+++ MyProject/MyHtmlAgility/Core/IMyHtmlAgilityBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.WebTesting;
+using MyProject.TestHelper;
+using NPOI.SS.Formula.Functions;
+
+namespace MyProject.MyHtmlAgility.Core
+{
+   
+    public interface IMyHtmlAgilityBase
+    {
+        string GetDocHtmlStr(string url, Encoding encoding);
+       
+
+    }
+}
--- MyProject/MyHtmlAgility/Core/MyHtmlAgilityBase.cs
+++ MyProject/MyHtmlAgility/Core/MyHtmlAgilityBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,13 +12,14 @@ using System.Windows.Forms;
 using HtmlAgilityPack;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.WebTesting;
+using MongoDB.Driver.Builders;
 using MyProject.TestHelper;
 using NPOI.SS.Formula.Functions;
 
 namespace MyProject.MyHtmlAgility.Core
 {
     [TestClass]
-    public abstract class MyHtmlAgilityBase 
+    public abstract class MyHtmlAgilityBase :IMyHtmlAgilityBase
     {
         public string LoginUrl { get; set; }
         public string Url { get; set; }
@@ -31,11 +34,46 @@ namespace MyProject.MyHtmlAgility.Core
         public virtual void InitBrowser()
         {
             ThisBrowser = new WebBrowser();
-           // ThisBrowser.op
-
         }
 
 
-   
+        public virtual string GetDocHtmlStr(string url, Encoding encoding=null )
+        {
+            if (  encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            string result = null;
+
+            WebResponse response = null;
+            StreamReader reader = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 2.0.50727)";
+                request.Accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, */*";
+
+                request.Method = "GET";
+                response = request.GetResponse();
+                reader = new StreamReader(response.GetResponseStream(), encoding);
+                result = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                result = "";
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+                if (response != null)
+                    response.Close();
+            }
+            return result;
+        }
+
+
+
     }
 }
