f60d50ed2629820dc8035b7b19ddc78cac546915
suijing <sj178220709@163.com>
2015-03-18 08:57:44 +0800

    fix

---
+++ MyMvcDemo/Scripts/Public/avalon/filters.js
@@ -0,0 +1,21 @@
+﻿
+define(function(require, exports, module) {
+   
+    var o = {};
+    o.getHahaContent = function(content) {
+        var text = $(content).find("p:first").text();
+        if (text && text != "") {
+            return text;
+        } else {
+            if ($(content).find("img").length > 0) {
+                return $(content).find("img").attr("src");
+            } else {
+                return content;
+            }
+        }
+    };
+   
+
+
+    module.exports = o;
+});
