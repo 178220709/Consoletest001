ef54a38486fc0af3d21d7ecf8de72d5192cf3ff7
suijing <sj178220709@163.com>
2015-05-26 00:58:01 +0800

    1

---
+++ Utils/Extention/LinqEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Suijing.Utils.Extention
+{
+    public static class LinqEx
+    {
+        public static IEnumerable<T> WhereAsync<T>(this IEnumerable<T> list,Func<T,Task<bool>> filter )
+        {
+            var newList = new List<T>();
+            list.ToList().ForEach(async a =>
+            {
+                var re = await filter(a);
+                if (re)
+                {
+                    newList.Add(a);
+                }
+            });
+
+            return newList;
+        }
+
+    }
+}
---
+++ Utils/sysTools/TestHelper.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Web;
+using System.Web.Hosting;
+
+
+
+namespace Suijing.Utils.sysTools
+{
+
+    public static class TestHelper
+    {
+        public static void InitCurrentContext()
+        {
+            if (HttpContext.Current != null) return;
+            Thread.GetDomain().SetData(".appPath", "c:\\inetpub\\wwwroot\\webapp\\");
+            Thread.GetDomain().SetData(".appVPath", "/");
+            TextWriter tw = new StringWriter();
+            HttpWorkerRequest wr = new SimpleWorkerRequest("/Home/app", "", tw);
+            HttpContext.Current = new HttpContext(wr);
+        }
+
+        public static string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public static void Log(string str)
+        {
+            var show = DateTime.Now.ToString("HH:mm:ss-fff") + str;
+            Console.WriteLine(show);
+            Trace.WriteLine(show);
+        }
+    }
+}
