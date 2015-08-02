a5dfa2eb1131481b956cc3a6b91a8f2f9763bb14
jsonsong <jsonsong@hotmail.com>
2015-08-03 00:12:54 +0800

    fix

---
+++ MaxthonExtension/js/vm_downloadtorrent.js
@@ -0,0 +1,20 @@
+﻿// ==UserScript==
+// @name         下载种子 去广告
+// @author      me
+// @description  
+// @namespace   
+// @icon        http://img.duoluohua.com/appimg/script_dupanlink_icon_48.png
+// @license     GPL version 3
+// @encoding    utf-8
+// @date        26/08/2013
+// @modified    05/09/2014
+// @include     http://down.happytogether2015.com/freeone/*
+
+// @grant       unsafeWindow
+// @run-at      document-end
+// @version     0.0.1
+// ==/UserScript==
+
+
+//document.getElementById("down_btn").removeAttribute("onclick");
+$("#down_btn").removeAttr("onclick");
