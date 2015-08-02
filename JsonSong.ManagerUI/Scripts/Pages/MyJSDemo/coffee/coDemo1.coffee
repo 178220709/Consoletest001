47957195db36f3e5e12e71dec34903c3a12a9a81
jsonsong <jsonsong@hotmail.com>
2015-08-03 00:10:33 +0800

    重构咯~

--- JsonSong.ManagerUI/Scripts/Pages/MyJSDemo/coffee/coDemo1.coffee
+++
@@ -1,9 +0,0 @@
-﻿# CoffeeScript
-list = [1, 2, 3, 4, 5]
-list2 = _.filter(list,(item)->item%2==0) 
-  
-mydouble = (x)-> x*2
-
-list3 = (mydouble x for x in list2)
-
-console x.toString() for x in list3
--- JsonSong.ManagerUI/Views/AvalonDemo/Sample.js
+++
@@ -1,8 +0,0 @@
-﻿/// use an xml comment tag to reference javascript file under test
-/// <reference path="/Scripts/MicrosoftAjax.js" />
-
-module("Sample.js - Sample QUnit tests suite");
-
-test("Sample test with expect number equals 1", 1, function() {
-    ok(true);
-});
